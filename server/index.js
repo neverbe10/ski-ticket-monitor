be2d6501fdf7cb189c6d53edf8bad52993ca7f94
Elaine Chen digitalocean <ela_ine@live.cn>
2021-01-07 09:33:13 +0000

    remove 0.0.0.0

--- server/index.js
+++ server/index.js
@@ -60,7 +60,7 @@ fastify.post("/subscribe", async (req, reply) => {
 });
 
 // Run the server!
-fastify.listen(process.env.PORT, '0.0.0.0', (err, address) => {
+fastify.listen(process.env.PORT, (err, address) => {
   if (err) throw err;
   fastify.log.info(`server listening on ${address}`);
 });
