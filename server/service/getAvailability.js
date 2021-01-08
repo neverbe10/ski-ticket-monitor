7819bfb43c31c2ab80ef94077a6052695c0c4b19
Elaine Chen <ela_ine@live.cn>
2021-01-07 19:47:17 -0800

    remove try catch

--- server/service/getAvailability.js
+++ server/service/getAvailability.js
@@ -9,50 +9,41 @@ const resortMap = {
 
 module.exports = async function getResortAvailbility(resort) {
   const result = [];
-  try {
-    const resortUrl = resortMap[resort];
-    if (resortUrl) {
-      const browser = await firefox.launch({ chromiumSandbox: false });
-      const context = await browser.newContext();
-      const page = await context.newPage();
-      page.route(/.*GetLiftTicketControlReservationInventory.*/, (route) => {
-        route
-          .request()
-          .response()
-          .then((response) => {
-            response
-              .body()
-              .then((b) => {
-                const arr = JSON.parse(b.toString());
-                result.push(...arr);
-              })
-              .catch((err) => {
-                throw err;
-              });
-          });
-        route.continue();
-      });
-
-      await page.goto(resortUrl, {
-        waitUntil: "networkidle",
-        timeout: 0,
-      });
-      await browser.close();
-
-      return result.sort(
-        (a, b) =>
-          new Date(a.InventoryDateTime).getTime() -
-          new Date(b.InventoryDateTime).getTime()
-      );
-    } else {
-      throw new Error("resort not exist");
-    }
-  } catch (e) {
-    console.error({ 
-      message: "get availability failed", 
-      e, 
-      resultLength : result.length,
+  const resortUrl = resortMap[resort];
+  if (resortUrl) {
+    const browser = await firefox.launch({ chromiumSandbox: false });
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    page.route(/.*GetLiftTicketControlReservationInventory.*/, (route) => {
+      route
+        .request()
+        .response()
+        .then((response) => {
+          response
+            .body()
+            .then((b) => {
+              const arr = JSON.parse(b.toString());
+              result.push(...arr);
+            })
+            .catch((err) => {
+              throw err;
+            });
+        });
+      route.continue();
     });
-    throw e;
+
+    await page.goto(resortUrl, {
+      waitUntil: "networkidle",
+      timeout: 0,
+    });
+    await browser.close();
+
+    return result.sort(
+      (a, b) =>
+        new Date(a.InventoryDateTime).getTime() -
+        new Date(b.InventoryDateTime).getTime()
+    );
+  } else {
+    throw new Error("resort not exist");
   }
 };
